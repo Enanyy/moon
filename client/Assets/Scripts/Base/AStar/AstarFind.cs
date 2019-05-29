594562f19365f613b9a6295e4896e1aa826b37d1
Enanyy <742216502@qq.com>
2019-05-29 21:27:43 +0800

    astar

---
+++ client/Assets/Scripts/Base/AStar/AstarFind.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+/// <summary>
+/// A*算法寻路
+/// </summary>
+public class AstarFind : MonoBehaviour
+{
+
+
+    AstarGrid grid;
+    void Start()
+    {
+        grid = GetComponent<AstarGrid>();
+        FindPath(grid.startPos, grid.endPos);
+    }
+
+
+
+
+
+    /// <summary>
+    /// 使用A*算法寻路
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="end"></param>
+    void FindPath(Vector2 start, Vector2 end)
+    {
+        //和A*算法无关，只是为了显示使用
+        int showFindNum = 1;
+
+        //开启列表
+        List<Cell> openLs = new List<Cell>();
+        //关闭列表
+        List<Cell> closeLs = new List<Cell>();
+
+        //起点
+        Cell startCell = grid.GetCell(start);
+        //终点
+        Cell endCell = grid.GetCell(end);
+        Debug.LogFormat("寻路开始,start({0}),end({1})!", start, end);
+
+        //将起点作为待处理的点放入开启列表，
+        openLs.Add(startCell);
+
+        //如果开启列表没有待处理点表示寻路失败，此路不通
+        while (openLs.Count > 0)
+        {
+            //遍历开启列表，找到消费最小的点作为检查点
+            Cell cur = openLs[0];
+            for (int i = 0; i < openLs.Count; i++)
+            {
+                if (openLs[i].fCost < cur.fCost && openLs[i].hCost < cur.hCost)
+                {
+                    cur = openLs[i];
+                }
+            }
+            Debug.Log("当前检查点：" + cur.ToString() + " 编号：" + showFindNum + "  open列表节点数量：" + openLs.Count);
+            //显示在界面，和A*算法无关
+            cur.obj.transform.Find("Num").GetComponent<Text>().text = showFindNum.ToString();
+            showFindNum++;
+
+            //从开启列表中删除检查点，把它加入到一个“关闭列表”，列表中保存所有不需要再次检查的方格。
+            openLs.Remove(cur);
+            closeLs.Add(cur);
+
+            //检查是否找到终点
+            if (cur == endCell)
+            {
+                Debug.Log("寻路结束!");
+                grid.CreatePath(cur);
+                return;
+            }
+
+            //根据检查点来找到周围可行走的点
+            //1.如果是墙或者在关闭列表中则跳过
+            //2.如果点不在开启列表中则添加
+            //3.如果点在开启列表中且当前的总花费比之前的总花费小，则更新该点信息
+            List<Cell> aroundCells = GetAllAroundCells(cur);
+            foreach (var cell in aroundCells)
+            {
+
+                if (cell.isWall || closeLs.Contains(cell))
+                    continue;
+
+                int cost = cur.gCost + GetDistanceCost(cell, cur);
+
+                if (cost < cell.gCost || !openLs.Contains(cell))
+                {
+                    cell.gCost = cost;
+                    cell.hCost = GetDistanceCost(cell, endCell);
+                    cell.parent = cur;
+                    Debug.Log("cell:" + cell.ToString() + "  parent:" + cur.ToString() + "  " + cell.PrintCost());
+                    if (!openLs.Contains(cell))
+                    {
+                        openLs.Add(cell);
+                    }
+
+                    //显示在界面，和A*算法无关
+                    cell.obj.transform.Find("Cost").GetComponent<Text>().text = cell.fCost.ToString();
+                }
+
+
+            }
+
+        }
+
+        Debug.Log("寻路失败!");
+    }
+
+
+    // 取得周围的节点
+    public List<Cell> GetAllAroundCells(Cell cell)
+    {
+        List<Cell> list = new List<Cell>();
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                // 如果是自己，则跳过
+                if (i == 0 && j == 0)
+                    continue;
+                int x = cell.x + i;
+                int y = cell.y + j;
+                // 判断是否越界，如果没有，加到列表中
+                if (x < grid.gridSize && x >= 0 && y < grid.gridSize && y >= 0)
+                {
+                    list.Add(grid.GetCell(new Vector2(x, y)));
+                }
+
+            }
+        }
+        return list;
+    }
+
+
+    /// <summary>
+    /// 估价函数，启发函数
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    int GetDistanceCost(Cell a, Cell b)
+    {
+        int cntX = Mathf.Abs(a.x - b.x);
+        int cntY = Mathf.Abs(a.y - b.y);
+        // 判断到底是那个轴相差的距离更远
+        if (cntX > cntY)
+        {
+            return 14 * cntY + 10 * (cntX - cntY);
+        }
+        else
+        {
+            return 14 * cntX + 10 * (cntY - cntX);
+        }
+    }
+
+}
---
+++ client/Assets/Scripts/Base/AStar/AstarGrid.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+/// <summary>
+/// 单独的格子
+/// </summary>
+public class Cell
+{
+    public int x;
+    public int y;
+    public bool isWall;
+    // 与起点的长度
+    public int gCost;
+    // 与目标点的长度
+    public int hCost;
+    // 总的路径长度
+    public int fCost
+    {
+        get { return gCost + hCost; }
+    }
+
+    // 父节点
+    public Cell parent = null;
+    public GameObject obj;
+
+    public Cell(int x, int y, bool isWall, GameObject obj)
+    {
+        this.x = x;
+        this.y = y;
+        this.isWall = isWall;
+        this.obj = obj;
+    }
+
+    public override string ToString()
+    {
+        return "(" + x + "," + y + ")";
+    }
+    public string PrintCost()
+    {
+        return "{ fCost:" + fCost + ",hCost:" + hCost + "}";
+    }
+
+}
+
+/// <summary>
+/// 地面的格子构建
+/// 这里只是简单的demo，没有优化也没有调整，只是纯展示A*算法
+/// </summary>
+public class AstarGrid : MonoBehaviour
+{
+
+    public GameObject CellAsset;
+    public GameObject PointAsset;
+
+    public int gridSize = 10;
+    int cellSize = 50;
+
+    List<string> wallList = new List<string>() { "5_5", "5_6", "5_7" };
+    public List<Cell> cells = new List<Cell>();
+
+    public Vector2 startPos = new Vector2(3, 6);
+    public Vector2 endPos = new Vector2(7, 6);
+
+    // Use this for initialization
+    void Awake()
+    {
+        CreateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void CreateGrid()
+    {
+
+        //创建格子；
+        for (int i = 0; i < gridSize; i++)
+        {
+            for (int j = 0; j < gridSize; j++)
+            {
+
+                GameObject obj = GameObject.Instantiate<GameObject>(CellAsset, transform);
+                RectTransform trans = obj.transform as RectTransform;
+                trans.anchoredPosition = new Vector2(i, j) * cellSize;
+                bool isWall = false;
+                trans.Find("Text").GetComponent<Text>().text = i + "," + j;
+                if (wallList.Contains(i + "_" + j))
+                {
+                    isWall = true;
+
+                    obj.GetComponent<Image>().color = Color.black;
+
+                }
+                Cell cell = new Cell(i, j, isWall, obj);
+                cells.Add(cell);
+            }
+        }
+
+        //创建起点
+        GameObject obj1 = GameObject.Instantiate<GameObject>(PointAsset, transform);
+        RectTransform trans1 = obj1.transform as RectTransform;
+        trans1.anchoredPosition = startPos * cellSize;
+        obj1.GetComponent<Image>().color = Color.red;
+        trans1.localScale = Vector3.one * 1.2f;
+        obj1.name = "start";
+
+        //创建终点
+        GameObject obj2 = GameObject.Instantiate<GameObject>(PointAsset, transform);
+        RectTransform trans2 = obj2.transform as RectTransform;
+        trans2.anchoredPosition = endPos * cellSize;
+        obj2.GetComponent<Image>().color = Color.green;
+        trans2.localScale = Vector3.one * 1.2f;
+        obj2.name = "end";
+    }
+
+
+    public Cell GetCell(Vector2 pos)
+    {
+        return cells.Find((c) => { return c.x == (int)pos.x && c.y == (int)pos.y; });
+    }
+
+
+    public void CreatePath(Cell endPos)
+    {
+        Cell cell = endPos;
+        while (cell != null)
+        {
+            GameObject obj1 = GameObject.Instantiate<GameObject>(PointAsset, transform);
+            RectTransform trans1 = obj1.transform as RectTransform;
+            trans1.anchoredPosition = cellSize * new Vector2(cell.x, cell.y);
+            obj1.GetComponent<Image>().color = new Color(Color.blue.r, Color.blue.g, Color.blue.b, 0.5f);
+            cell = cell.parent;
+        }
+
+    }
+
+}
