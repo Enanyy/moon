3c4841acfcb0c382fe970a6852238da1fa20782e
Enanyy <742216502@qq.com>
2019-06-17 13:57:05 +0800

    Asset

--- client/Assets/Scripts/Asset/AssetPath.cs
+++ client/Assets/Scripts/Asset/AssetPath.cs
@@ -3,132 +3,72 @@ using System.Collections.Generic;
 using System.IO;
 using System.Xml;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
-public class Asset
-{
-    public string name;
-    public string path;
-    public long size;
-    public string md5;
-
-    public void ToXml(XmlNode parent)
-    {
-        XmlDocument doc;
-        if (parent.ParentNode == null)
-        {
-            doc = (XmlDocument)parent;
-        }
-        else
-        {
-            doc = parent.OwnerDocument;
-        }
-        XmlElement node = doc.CreateElement("asset");
-        parent.AppendChild(node);
-
-        XmlAttribute name = doc.CreateAttribute("name");
-        name.Value = this.name;
-
-        node.Attributes.Append(name);
-
-        XmlAttribute path = doc.CreateAttribute("path");
-        path.Value = this.path;
-
-        node.Attributes.Append(path);
-
-        XmlAttribute size = doc.CreateAttribute("size");
-        size.Value = this.size.ToString();
-
-        node.Attributes.Append(size);
-
-        XmlAttribute md5 = doc.CreateAttribute("md5");
-        md5.Value = this.md5;
-
-        node.Attributes.Append(md5);
-    }
-
-    public void FromXml(XmlElement element)
-    {
-        name = element.GetAttribute("name");
-        path = element.GetAttribute("path");
-        size = element.GetAttribute("size").ToInt64Ex();
-        md5 = element.GetAttribute("md5");
-    }
-}
 
 public  class AssetPath
 {
+    #region Inner Class
+    public class Asset
+    {
+        public string name;
+        public string path;
+        public long size;
+        public string md5;
+
+        public void ToXml(XmlNode parent)
+        {
+            XmlDocument doc;
+            if (parent.ParentNode == null)
+            {
+                doc = (XmlDocument)parent;
+            }
+            else
+            {
+                doc = parent.OwnerDocument;
+            }
+            XmlElement node = doc.CreateElement("asset");
+            parent.AppendChild(node);
+
+            XmlAttribute name = doc.CreateAttribute("name");
+            name.Value = this.name;
+
+            node.Attributes.Append(name);
+
+            XmlAttribute path = doc.CreateAttribute("path");
+            path.Value = this.path;
+
+            node.Attributes.Append(path);
+
+            XmlAttribute size = doc.CreateAttribute("size");
+            size.Value = this.size.ToString();
+
+            node.Attributes.Append(size);
+
+            XmlAttribute md5 = doc.CreateAttribute("md5");
+            md5.Value = this.md5;
+
+            node.Attributes.Append(md5);
+        }
+
+        public void FromXml(XmlElement element)
+        {
+            name = element.GetAttribute("name");
+            path = element.GetAttribute("path");
+            size = element.GetAttribute("size").ToInt64Ex();
+            md5 = element.GetAttribute("md5");
+        }
+    }
+    #endregion
     public static Dictionary<string, Asset> assets = new Dictionary<string, Asset>();
 #if UNITY_EDITOR
     [RuntimeInitializeOnLoadMethod]
-    private static void Init()
+    public static void Init()
     { 
         string path = Application.dataPath + "/asset.txt";
         string xml = File.ReadAllText(path);
 
         FromXml(xml);
     }
-    [InitializeOnLoadMethod]
-    private static void InitEditor()
-    {
-        Debug.Log("AssetPath Init");
-        Init();
-        Editor.finishedDefaultHeaderGUI -= OnPostHeaderGUI;
-        Editor.finishedDefaultHeaderGUI += OnPostHeaderGUI;
-    }
-
-    static void OnPostHeaderGUI(Editor editor)
-    {
-        if (editor.target == null)
-        {
-            return;
-        }
-        string assetPath = AssetDatabase.GetAssetPath(editor.target);
-
-        if (string.IsNullOrEmpty(assetPath) 
-            || assetPath.EndsWith(".cs")
-            || assetPath.StartsWith("Assets/Resources/") == false
-            )
-        {
-            return;
-        }
-
-        Asset asset = null;
-        string name = Path.GetFileNameWithoutExtension(assetPath);
-        string path = assetPath.Replace("Assets/Resources/", "").ToLower();
-        var it = assets.GetEnumerator();
-        while (it.MoveNext())
-        {
-            if (it.Current.Value.path == path)
-            {
-                asset = it.Current.Value;break;
-            }
-        }
-
-
-        GUILayout.BeginHorizontal();
-
-
-        if (GUILayout.Toggle(asset!= null, "Asset", GUILayout.ExpandWidth(false)) == false)
-        {
-            assets.Remove(asset.name);
-
-            asset = null;
-        }
-
-        if (asset != null)
-        {
-            var assetName = EditorGUILayout.DelayedTextField(asset.name, GUILayout.ExpandWidth(true));
-            if (assetName != asset.name)
-            {
-                asset.name = assetName;
-            }
-        }
-        GUILayout.EndHorizontal();
-    }
-
+   
 #endif
     public static void FromXml(string xml)
     {
--- client/Assets/Scripts/Editor/AssetTool.cs
+++ client/Assets/Scripts/Editor/AssetTool.cs
@@ -6,27 +6,113 @@ using UnityEngine;
 
 public static class AssetTool
 {
+    [InitializeOnLoadMethod]
+    private static void InitEditor()
+    {
+        Debug.Log("AssetPath Init");
+        Debug.Log(Application.dataPath);
+        AssetPath.Init();
+        Editor.finishedDefaultHeaderGUI -= OnPostHeaderGUI;
+        Editor.finishedDefaultHeaderGUI += OnPostHeaderGUI;
+        EditorApplication.quitting -= SaveAsset;
+        EditorApplication.quitting += SaveAsset;
+    }
+
+    static void OnPostHeaderGUI(Editor editor)
+    {
+        if (editor.target == null)
+        {
+            return;
+        }
+        string assetPath = AssetDatabase.GetAssetPath(editor.target);
+
+        if (string.IsNullOrEmpty(assetPath)
+            || assetPath.EndsWith(".cs")
+            || assetPath.StartsWith("Assets/Resources/") == false
+            )
+        {
+            return;
+        }
+
+        AssetPath.Asset asset = null;
+        string name = Path.GetFileNameWithoutExtension(assetPath);
+        string path = assetPath.Replace("Assets/Resources/", "").ToLower();
+        var it = AssetPath.assets.GetEnumerator();
+        while (it.MoveNext())
+        {
+            if (it.Current.Value.path == path)
+            {
+                asset = it.Current.Value; break;
+            }
+        }
+
+        if (asset == null)
+        {
+            string fullPath = Application.dataPath + "/Resources/" + path;
+            if (File.Exists(fullPath))
+            {
+                string md5 = MD5Hash.Get(File.ReadAllBytes(fullPath));
+                it = AssetPath.assets.GetEnumerator();
+                while (it.MoveNext())
+                {
+                    if (it.Current.Value.md5 == md5)
+                    {
+                        asset = it.Current.Value;
+                        if (asset.path != path)
+                        {
+                            asset.path = path;
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        GUILayout.BeginHorizontal();
+
+
+        if (GUILayout.Toggle(asset != null, "Asset", GUILayout.ExpandWidth(false)) == false)
+        {
+            AssetPath.assets.Remove(asset.name);
+
+            asset = null;
+        }
+
+        if (asset != null)
+        {
+            var assetName = EditorGUILayout.DelayedTextField(asset.name, GUILayout.ExpandWidth(true));
+            if (assetName != asset.name)
+            {
+                AssetPath.assets.Remove(asset.name);
+                asset.name = assetName;
+                AssetPath.assets.Add(asset.name, asset);
+            }
+        }
+        GUILayout.EndHorizontal();
+    }
+
     [MenuItem("Tools/生成资源配置")]
     static void GenAsset()
     {
-   
         DirectoryInfo dirInfo = new DirectoryInfo(Application.dataPath + "/Resources/r/");
         FileInfo[] files = dirInfo.GetFiles("*.*", SearchOption.AllDirectories);
 
         AssetPath.Clear();
-       
-        for (int i = 0; i < files.Length;++i)
+
+        for (int i = 0; i < files.Length; ++i)
         {
-            if(files[i].Name.EndsWith(".meta"))
+            if (files[i].Name.EndsWith(".meta"))
             {
                 continue;
             }
-            string fullPath = files[i].DirectoryName+"/"+ files[i].Name;
+            string fullPath = files[i].DirectoryName + "/" + files[i].Name;
             fullPath = fullPath.Replace("\\", "/");
 
-            string assetPath = fullPath.Substring(fullPath.IndexOf("/r/") +1).ToLower();
-           
-            Asset asset = new Asset();
+            string assetPath = fullPath.Substring(fullPath.IndexOf("/r/") + 1).ToLower();
+
+            AssetPath.Asset asset = new AssetPath.Asset();
 
             asset.name = files[i].Name;
             asset.path = assetPath;
@@ -38,6 +124,11 @@ public static class AssetTool
             AssetPath.assets.Add(asset.name, asset);
         }
 
+        SaveAsset();
+    }
+
+    static void SaveAsset()
+    {
         string assetXmlFile = Application.dataPath + "/asset.txt";
 
         StreamWriter writerXml = new StreamWriter(assetXmlFile);
